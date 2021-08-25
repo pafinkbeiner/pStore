1370197d015a2e747d8d89b7a0c9405eb4c8c84e
asd <sad>
2021-08-25 15:44:14 +0200

    finished

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -1,9 +1,28 @@
 import { Router} from "express";
+import jwt from "jsonwebtoken"
 
 let authRouter = Router();
 
+interface authFunction<User>{
+    (user: User): User| undefined
+}
+
 authRouter.post("/", (req, res) => {
-    res.json("Login")
+
+    let user = req.body;
+
+    // impement own authentication logic
+    const authUser: authFunction<any> = (user:any) => {user};
+
+    const result = authUser(user);
+
+    if(result){
+        let secret: any = process.env.JWT_SECRET;
+        let token = jwt.sign({user: result}, secret)
+        res.json({token: token})
+    }else{
+        res.sendStatus(400);
+    }
 })
 
 export default authRouter;
